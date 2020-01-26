bde24e16e214d49fce331d53aa6e37f7c1f6ef58
Lagicrus <Lagicrus@users.noreply.github.com>
2020-01-26 22:32:12 +0000

    remove unused route

--- api/check_website.py
+++ api/check_website.py
@@ -81,33 +81,3 @@ async def get_check_website(request):
     # checks[url]["redirects"] = await api.helpers.redirect_gatherer(url, request.app.session)
 
     return sanic.response.json({"processed": checks})
-
-
-@bp.route("/check_redirect", methods=["GET"])
-async def get_check_redirect(request):
-    try:
-        if "website" in request.args:
-            search_query = request.args["website"][0]
-        else:
-            search_query = None
-        if search_query is None or search_query == "":
-            search_query = request.json.get("website")
-    except (AttributeError, KeyError):
-        error = (
-            "Please specify the search query under the 'website' key."
-        )
-        return sanic.response.json({"error": error}, status=400)
-
-    if not await api.helpers.validate_url(search_query):
-        return sanic.response.json({"status": "Invalid URL"},
-                                   status=400)
-
-    url = search_query
-
-    checks = await api.helpers.redirect_gatherer(url, request.app.session)
-    log.info(checks)
-    for x in checks:
-        log.info(x)
-        log.info(type(x))
-
-    return sanic.response.json({"processed": checks})
