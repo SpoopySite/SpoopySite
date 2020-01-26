2f31f7245d1bac1b999dae834f9da74569b958c8
SnowyLuma <SnowyLuma@gmail.com>
2020-01-27 00:40:38 +0100

    Simplify footer and use router links for same-page links

--- spoopy-site/src/footer/Footer.js
+++ spoopy-site/src/footer/Footer.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 class Footer extends Component {
   render() {
     return (
-      <footer className="page-footer">
-        <div className="footer-copyright">
-          <div className="container">
-            <p>Inspired by <a href="https://github.com/spoopy-link/server">spoopy</a>. Source at <a
-              href="https://github.com/Lagicrus/spoopy-python">Github</a></p>
-            <p className="subFooter">Docs can be found <a href="/docs">here</a>. Homepage
-            located <a href="/">here</a></p>
-          </div>
-        </div>
+      <footer>
+        <p>
+          <Link to="/">Home</Link>
+          <Link to="/docs">Docs</Link>
+          <a href="https://github.com/Lagicrus/spoopy-python">GitHub</a>
+        </p>
+        <p className="inspiration">
+          Inspired by <a href="https://github.com/spoopy-link/server">spoopy link</a>
+        </p>
       </footer>
     );
   }
