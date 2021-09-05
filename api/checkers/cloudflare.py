05ae8e4355698037424ebbe3220f847075f435cd
Sam <Lagicrus@users.noreply.github.com>
2021-09-05 20:48:53 +0100

    [cloudflare] Handles ports on domains better
    Fixes SANIC-G
    Fixes SANIC-H

--- api/checkers/cloudflare.py
+++ api/checkers/cloudflare.py
@@ -3,12 +3,17 @@ import dns.rrset
 from dns.asyncresolver import Resolver
 from dns.resolver import NXDOMAIN
 import logging
+import tld
 
 log = logging.getLogger(__name__)
 
 
 async def check(domain: str, rtype: str = "A") -> dns.rrset.RRset:
     log.info(f"CloudFlare checking: '{domain}'")
+    if domain.count(":") > 0:
+        processed_url = tld.get_fld(domain, fix_protocol=True)
+        log.info(f"CloudFlare processed: '{domain}' to '{processed_url}'")
+        domain = processed_url
     rs = Resolver()
     rs.nameservers = ["1.1.1.2"]
     rs.timeout = 5
