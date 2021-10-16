b64e1c5ae5ad91074d71c530a49c1ee018c0d665
Sam <Lagicrus@users.noreply.github.com>
2021-10-16 22:07:38 +0100

    [linkvertise] Refactor to handle LV better

--- api/handlers/linkvertise.py
+++ api/handlers/linkvertise.py
@@ -12,26 +12,24 @@ from app.useragents import get_random_user_agent
 log = logging.getLogger(__name__)
 
 
-def linkvertise_domains():
-    with open("api/handlers/data/linkvertise.json", "r") as file:
-        data = json.load(file)
-    return data
-
-
-async def get_link_id(parsed: ParseResult, session: aiohttp.client.ClientSession):
-    url = parsed.path
-    url = url.lstrip("/")
-    url = url.rstrip("/")
-
-    headers = {
+def headers():
+    return {
         "accept": "application/json",
         "user-agent": get_random_user_agent(),
         "referer": "https://linkvertise.com/",
         "origin": "https://linkvertise.com",
     }
 
+
+def linkvertise_domains():
+    with open("api/handlers/data/linkvertise.json", "r") as file:
+        data = json.load(file)
+    return data
+
+
+async def get_link_id(parsed: ParseResult, session: aiohttp.client.ClientSession, url: str):
     async with session.get(f"https://publisher.linkvertise.com/api/v1/redirect/link/static/{url}",
-                           headers=headers,
+                           headers=headers(),
                            allow_redirects=False
                            ) as resp:
         json_content: dict = await resp.json()
@@ -53,16 +51,24 @@ def get_serial(link_id: int):
     return base64.b64encode(data_string).decode("utf-8")
 
 
-async def get_target(link_id: int, session: aiohttp.client.ClientSession, parsed: ParseResult):
+async def get_target(link_id: int, session: aiohttp.client.ClientSession, parsed: ParseResult, url: str):
     serial = get_serial(link_id)
-    async with session.post(f"https://publisher.linkvertise.com/api/v1/redirect/link{parsed.path}/target", json={
+    async with session.post(f"https://publisher.linkvertise.com/api/v1/redirect/link/{url}/target", json={
         "serial": serial
-    }, headers={"User-Agent": get_random_user_agent()}) as resp:
+    }, headers=headers()) as resp:
         json_content: dict = await resp.json()
     return json_content
 
 
 async def linkvertise(parsed: ParseResult, session: aiohttp.client.ClientSession):
-    link_id = await get_link_id(parsed, session)
-    fetch_target = await get_target(link_id, session, parsed)
+    url = parsed.path
+
+    url = url.lstrip("/download/")
+    url = url.lstrip("/")
+
+    log.info(url)
+
+    link_id = await get_link_id(parsed, session, url)
+    fetch_target = await get_target(link_id, session, parsed, url)
+    log.info(f"FT: {fetch_target}")
     return fetch_target["data"]["target"]
